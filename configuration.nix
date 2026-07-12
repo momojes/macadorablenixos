054e9cdbd0de113fab374f552c0f68cbc1f3928b
momojes <thingsaboutjleyva@gmail.com>
2026-07-12 12:22:51 -0700

    Tidied Up configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -22,11 +18,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "macAdorable"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -64,44 +55,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    rustc
-    cargo
-    rustfmt
-    clippy
-    rust-analyzer
-    hugo
-    gleam
-    clang
-    xrandr
-    pkgs.github-cli
-    gcc
-    gnumake
-    ncurses
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # Modern Audio Stack
   security.rtkit.enable = true;
   services.pipewire = {
@@ -130,27 +83,6 @@
 		exit 1
 		fi
 	    '';
-  };
-
-  # Garbage Collection
-  nix = {
-  	settings.auto-optimise-store = true;
-
-	gc = {
-		automatic = true;
-		dates = "weekly";
-		options = "--delete-older-than 14d";
-	  };
-  };
-
-  # Firefox
-  programs.firefox = {
-    enable = true;
-
-    preferences = {
-      "browser.download.dir" = "/home/momo/downloads";
-      "browser.download.folderList" = 2;
-    };
   };
 
   # This value determines the NixOS release from which the default
