535b0f72de38928c7e90d1aeff0a8c88dbf1b37b
momojes <thingsaboutjleyva@gmail.com>
2026-07-21 23:45:07 -0700

    Updated Maintenance and GC Schedule in Maintenance.nix

--- modules/maintenance.nix
+++ modules/maintenance.nix
@@ -1,13 +1,32 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  nix = {
-    settings.auto-optimise-store = true;
+  nix.settings.auto-optimise-store = true;
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
+  systemd.services.nix-gc-keep-ten = {
+    description = "Keep the latest 10 NixOS generations";
+
+    serviceConfig = {
+    	Type = "oneshot";
     };
-  };
+
+    script = ''
+    	   ${pkgs.nix}/bin/nix-env \
+	   --profile /nix/var/nix/profiles/system \
+	   --delete-generations + 10
+	   ${pkgs.nix}/bin/nix-store --gc
+	'';
+    };
+
+    systemd.timers.nix-gc-keep-ten = {
+      description = "Weekly NixOS generation cleanup";
+      wantedBy= [ "timers.target" ];
+      timerConfig = {
+      		  OnCalendar = "weekly";
+		  Persistent = true;
+		  Unit = "nix-gc-keep-ten.service";
+		 };
+    };
+
+    boot.loader.systemd-boot.configurationLimit =10;
 }
