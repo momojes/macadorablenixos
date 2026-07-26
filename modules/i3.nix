221aa572863d8ef5bdc69f2a479f97e5ff3ba356
momojes <thingsaboutjleyva@gmail.com>
2026-07-26 10:25:27 -0700

    Created i3.nix for modular versioning

---
+++ modules/i3.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+    service.xserver = {
+        enable = true;
+
+	xkb = {
+	    layout = "us";
+	    options = "ctrl:nocaps"
+	};
+
+	windowManager.i3 = {
+	    enable = true;
+
+	    extraPackages = with pkgs; [
+	    	dmenu
+		i3status
+	    ];
+	};
+    };
+
+    services.picom = {
+        enable = true;
+	backend = "glx";
+	vSync = true;
+    };
+}
