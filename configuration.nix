fce552bdce2cc1a4470315d07194ba5044cc233b
momojes <thingsaboutjleyva@gmail.com>
2026-07-27 14:40:24 -0700

    Enabled Experimental Features (nix-command & flakes)

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,12 @@
 	    '';
   };
 
+  # Enable experimental features
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
